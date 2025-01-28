74327a0d07f57dfd3e732fc6f8e8b7b897bda6f4
Alexander Fuchs <alexanderfuchs@Alexanders-MBP.fritz.box>
2025-01-28 20:03:32 +0100

    text features

--- finetune_text_features.py
+++ finetune_text_features.py
@@ -13,8 +13,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "mps")
 def finetune_textual_embeddings(number_of_epochs, batch_size):
     predictor = visual_attractiveness.VisualAttractiveness()
 
-    text_features = torch.load("weights/text_features_mebeauty_100k.pth")
-    text_features = nn.Parameter(torch.tensor(text_features, dtype=torch.float32, requires_grad=True))
+    text_features = predictor.encode_captions(['attractive', 'unattractive'])
+    text_features = nn.Parameter(torch.tensor(text_features, dtype=torch.float32), requires_grad=True)
 
     mse_loss = nn.MSELoss()
     optimizer = optim.Adam([text_features], lr=1e-3)
@@ -23,12 +23,14 @@ def finetune_textual_embeddings(number_of_epochs, batch_size):
     image_path_to_score = list(image_path_to_score.items())
     image_path_to_score = DataLoader(image_path_to_score, batch_size=batch_size, collate_fn=lambda batch: batch, shuffle=True)
 
+    image_path_to_latent = mebeauty.load_image_path_to_latent(predictor)
+
     losses = []
     for epoch_index in range(number_of_epochs):
         for batch in image_path_to_score:
             image_paths = list(zip(*batch))[0]
 
-            image_path_to_latent = mebeauty.load_image_path_to_latent(predictor, image_paths)
+            #image_path_to_latent = mebeauty.load_image_path_to_latent(predictor, image_paths)
 
             latent_to_score = [(image_path_to_latent[image_path], score) for image_path, score in batch if image_path in list(image_path_to_latent.keys())]
 
@@ -55,9 +57,9 @@ def finetune_textual_embeddings(number_of_epochs, batch_size):
 
         print(f"Epoch [{epoch_index + 1}], Loss: {torch.mean(torch.tensor(losses)):.4f}")
 
-    torch.save(text_features, "weights/text_features_mebeauty_200k.pth")
+    torch.save(text_features, "weights/text_features_mebeauty_100k.pth")
     print("Text Features Mebeauty saved!")
 
 
 if __name__ == "__main__":
-    finetune_textual_embeddings(20, 64)
+    finetune_textual_embeddings(40, 64)
